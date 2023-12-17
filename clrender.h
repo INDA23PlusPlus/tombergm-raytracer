da86e1ffbccb61a41c318a8420fdf8e556590fd1
glank <glankk@users.noreply.github.com>
2023-12-17 13:12:47 +0100

    Add runtime device selection, framerate in window title

--- clrender.h
+++ clrender.h
@@ -6,10 +6,10 @@
 #include "vec.h"
 #include "vp.h"
 
-void clrender_init(unsigned char *pb, const vp_t *vp, const scene_t *scene);
-void clrender_commit(	cam_t *cam, vp_t *vp,
+int	clrender_init(unsigned char *pb, const vp_t *vp, const scene_t *scene);
+void	clrender_commit(cam_t *cam, vp_t *vp,
 			unsigned char *pb, vec3_t *sb,
 			int sn);
-void clrender_wait(void);
+void	clrender_wait(void);
 
 #endif
